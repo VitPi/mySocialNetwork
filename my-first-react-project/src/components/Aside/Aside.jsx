62d48768aa0b729be2d4303814e2b43f454d00cd
Vitali Pilipchuk <cpaaaik@gmail.com>
2021-03-29 08:32:38 +0300

    bla

--- my-first-react-project/src/components/Aside/Aside.jsx
+++ my-first-react-project/src/components/Aside/Aside.jsx
@@ -11,30 +11,10 @@ const Aside = () => {
                 <Users/>
                 <Users/>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores cumque delectus dolorem dolores
-                    facere ipsa iure magni, maiores mollitia nemo neque, perspiciatis placeat possimus quos rerum
-                    similique sint suscipit tenetur ullam voluptates. Cupiditate eaque excepturi impedit laudantium
-                    minima omnis, qui quidem soluta. Atque commodi cupiditate eligendi excepturi quisquam soluta ut.</p>
+
+                
 
 
-                <Users/>
-                <Users/>
-                <Users/>
-                <Users/>
-                <Users/>
-                <Users/>
-                <Users/>
-                <Users/>
-                <Users/>
-                <Users/>
-                <Users/>
-                <Users/>
-                <Users/>
-                <Users/>
-                <Users/>
-                <Users/>
-                <Users/>
-                <Users/>
             </div>
         </aside>
     )
