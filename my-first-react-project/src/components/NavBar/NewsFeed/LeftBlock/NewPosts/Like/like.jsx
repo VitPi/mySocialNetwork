b0516edb51492adbdec6d9ab40efefde513b9bcc
ViTaLi <cpaaaik@gmail.com>
2021-04-07 13:52:38 +0300

    soci

--- my-first-react-project/src/components/NavBar/NewsFeed/LeftBlock/NewPosts/Like/like.jsx
+++ my-first-react-project/src/components/NavBar/NewsFeed/LeftBlock/NewPosts/Like/like.jsx
@@ -5,7 +5,13 @@
 
 import like from './img/01.png'
 import l from './like.module.css'
+import Users from "../../../../../Aside/Users/Users";
 
+
+let objUser = [
+    {name: 'Royal Clark', say: '4 hour ago'},
+    {name: 'Keith Flores', say: '2 yer ago'},
+].map(u => <Users name={u.name} say={u.say}/>)
 
 
 const Like = () => {
@@ -24,11 +30,17 @@ const Like = () => {
 
                 </figcaption>
             </figure>
+
+            {objUser}
+
+
         </footer>
     );
 };
 
 export default Like;
+
+
 
 
 
