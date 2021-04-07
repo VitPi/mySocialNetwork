21f0394584d975f9304f11614949f6d18f24b69d
ViTaLi <cpaaaik@gmail.com>
2021-04-07 12:42:23 +0300

    soci

--- my-first-react-project/src/components/NavBar/NewsFeed/LeftBlock/NewPosts/Like/like.jsx
+++ my-first-react-project/src/components/NavBar/NewsFeed/LeftBlock/NewPosts/Like/like.jsx
@@ -6,14 +6,22 @@
 import like from './img/01.png'
 import l from './like.module.css'
 
+
+
 const Like = () => {
     return (
         <footer className={l.block}>
             <figure>
                 <img src={like} alt="img"/>
                 <figcaption>
-                    <span>10</span> <p>Likes</p>
-                    <span>20</span> <p>Comment</p>
+                    <a href="#">
+                        <span>10</span> Likes
+                    </a>
+
+                    <a href="#">
+                        <span>20</span> Comment
+                    </a>
+
                 </figcaption>
             </figure>
         </footer>
